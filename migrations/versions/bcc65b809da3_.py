75262ba7b6e09d742ad351676f08449664413a47
Brian <mulindijrn@gmail.com>
2024-01-18 12:02:26 +0300

    Initial commit

---
+++ migrations/versions/bcc65b809da3_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: bcc65b809da3
+Revises: 
+Create Date: 2024-01-18 11:47:00.939663
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bcc65b809da3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
